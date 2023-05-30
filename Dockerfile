79c241a4a174db22fd594e50cee285ad469fb5c4
Alex Ashley <alexashley@users.noreply.github.com>
2023-05-30 18:16:53 -0400

    feat: add sample dependency to demonstrate SBOM (#7)

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:1.20-alpine as builder
 
 WORKDIR /app
 
-COPY main.go go.mod ./
+COPY main.go go.mod go.sum ./
 RUN go build -o server ./
 
 ###
--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("PONG"))
 	})
+
+	http.HandleFunc("/id", func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New()
+		w.Write([]byte(fmt.Sprintf(`{"id": "%s"}`, id.String())))
+	})
+
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	})
