05556d4c6c7fe9f5ea32ae59c3ddb99a84bc559a
Ryan Vance <ryanv@liatrio.com>
2023-06-01 12:17:05 -0500

    demo unapproved (#19)

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("PING"))
+		w.Write([]byte("PONG"))
 	})
 
 	http.HandleFunc("/id", func(w http.ResponseWriter, r *http.Request) {
